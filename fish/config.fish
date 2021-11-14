2e14627d2a70da09519555014b9fb46578c4d4e6
Fabio Lusha <f.lusha@studenti.unibs.it>
2021-11-14 16:44:25 +0100

    adding erlang vim support

--- fish/config.fish
+++ fish/config.fish
@@ -1,28 +1,29 @@
-set fish_greeting ""
-
-set -x JAVA_HOME /usr/bin/java
+set -x JAVA_HOME /usr/lib/jvm/java-11-openjdk
 
 #the GUROBI envirnoment varaible
 set -x GUROBI_HOME /opt/gurobi901/linux64
-set -x PATH PATH $GUROBI_HOME/bin $PATH
-set -x LIBRARY_PATH "$GUROBI_HOME/lib"
-set -x LD_LIBRARY_PATH "$GUROBI_HOME/lib"
-set -x CLASSPATH "$LD_LIBRARY_PATH:$CLASSPATH"
-set -x CLASSPATH "/opt/colt/lib/colt.jar:/opt/colt/lib/concurrent.jar:$CLASSPATH"
+fish_add_path $GUROBI_HOME/bin 
+set -x LIBRARY_PATH $GUROBI_HOME/lib
+set -x LD_LIBRARY_PATH $GUROBI_HOME/lib
 
-#making LaTeX command working
-set -x PERL5LIB /usr/share/tlpkg
-set -x PATH /usr/share/texmf-dist/scripts/texlive $PATH
-
-# rust configurations
-set -x PATH $HOME/.cargo/bin $PATH
+set -x CLASSPATH $LD_LIBRARY_PATH 
+set -x CLASSPATH /opt/colt/lib/colt.jar $CLASSPATH
+set -x CLASSPATH /opt/colt/lib/concurrent.jar $CLASSPATH
 
 set -x PYTHONPATH /usr/lib/python2.7/site-packages
 set -x PYTHONPATH /usr/lib/python3.8/site-packages $PYTHONPATH
 
+# set texlive env
+#fish_add_path /usr/share/texmf-dist/scripts/texlive
 # set default editor
 set -x VISUAL nvim
 set -x EDITOR $VISUAL
+
+# rebar3
+fish_add_path /home/fabiolusha/.cache/rebar3/bin 
+
+# anaconda
+fish_add_path --append /home/fabiolusha/Programs/anaconda3/bin
 
 alias max="sudo cpupower frequency-set -u 3.4GHz"
 alias min="sudo cpupower frequency-set -u 1GHz"
--- fish/functions/unibs.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function unibs --wraps='cd /home/fabiolusha/Desktop/DeskArch-10-20/unibs' --description 'alias unibs cd /home/fabiolusha/Desktop/DeskArch-10-20/unibs'
-  cd /home/fabiolusha/Desktop/DeskArch-10-20/unibs $argv;
-end
